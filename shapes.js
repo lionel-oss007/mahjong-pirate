be8c59d517b53d256573dcf4b8a2c53ed7684c92
lionel-oss007 <carrerlionel@gmail.com>
2026-09-09 21:54:06 +0200

    Actualise le cache du decor pirate - phase 3

--- shapes.js
+++ shapes.js
@@ -14,7 +14,7 @@
   if (!document.querySelector('link[data-home-visual]')) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'home-visual.css?v=20260909-1';
+    link.href = 'home-visual.css?v=20260909-3';
     link.dataset.homeVisual = '1';
     document.head.appendChild(link);
   }
